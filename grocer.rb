2b052a58232ecd018a31e800272e051476c42224
Aniket B <aniket.b@outlook.com>
2019-09-10 17:35:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,17 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do |item|
+    if hash[item.keys[0]]
+      hash[item.keys[0]][:count] += 1
+    else
+      hash[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
